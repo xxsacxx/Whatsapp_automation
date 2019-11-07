0be524c1ad1b5a01b7e6b4c2c995826e224a1433
SACHIN TRIPATHI <37820547+xxsacxx@users.noreply.github.com>
2019-11-08 02:38:22 +0530

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -2,8 +2,8 @@ import schedule
 import time
 from ip_chk import ip_chk
 from gcal import getlatestevnt
-
-schedule.every().day.at("02:29").do(getlatestevnt)
+#you will get last event added to your calendar at morning 7
+schedule.every().day.at("07:00").do(getlatestevnt)
 while True:
     schedule.run_pending()
     time.sleep(1)
